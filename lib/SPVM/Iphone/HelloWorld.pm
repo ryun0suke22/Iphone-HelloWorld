21c62400ed045adacca4b804309d6ec18ebe44b5
ryun0suke22 <jhan.k.police@gmail.com>
2025-09-29 20:08:35 +0900

    initial

---
+++ lib/SPVM/Iphone/HelloWorld.pm
@@ -0,0 +1,71 @@
+package SPVM::Iphone::HelloWorld;
+
+our $VERSION = "0.001";
+
+1;
+
+=encoding utf8
+
+=head1 Name
+
+SPVM::Iphone::HelloWorld - Short Description
+
+=head1 Description
+
+Iphone::HelloWorld class in L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use Iphone::HelloWorld;
+
+=head1 Details
+
+
+
+=head1 Super Class
+
+
+
+=head1 Interfaces
+
+
+
+=head1 Enumerations
+
+
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Well Known Child Classes
+
+
+
+=head1 See Also
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+[--user-name] C<[--user-email]>
+
+=head1 Copyright & License
+
+Copyright (c) 2025 [--user-name]
+
+MIT License
+
